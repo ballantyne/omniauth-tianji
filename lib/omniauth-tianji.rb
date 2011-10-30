b5ff3f81420c081f56389526a63a04b8a576b122
Scott Ballantyne <ussballantyne@gmail.com>
2011-10-30 16:06:22 +0800

    more

--- lib/omniauth-tianji.rb
+++ lib/omniauth-tianji.rb
@@ -1,7 +1,2 @@
 require "omniauth-tianji/version"
-
-module Omniauth
-  module Tianji
-    # Your code goes here...
-  end
-end
+require "omniauth/strategies/tianji"
---
+++ lib/omniauth/strategies/tianji.rb
@@ -0,0 +1,38 @@
+require 'omniauth-oauth2'
+module OmniAuth
+  module Strategies
+    class Tianji < OmniAuth::Strategies::OAuth2
+      option :client_options, {
+        :site          => 'https://login.tianji.com/',
+        :authorize_url => '/authz/oauth/authorize',
+        :token_url     => '/authz/oauth/token',
+        :token_method  => :post
+      }
+      def request_phase
+        super
+      end
+
+      uid { raw_info['uid'] }
+
+      info do
+        {
+          "uid" => raw_info["uid"], 
+          "gender"=> (raw_info['gender'] == '0' ? 'Male' : 'Female'), 
+          "image"=>raw_info['logo50'],
+          'name' => raw_info['name'],
+          'urls' => {
+            'Kaixin' => "http://www.kaixin001.com/"
+          }
+        }
+      end
+
+      def raw_info
+        @raw_info ||= MultiJson.decode(access_token.get("/users/me.json?access_token=#{@access_token.token}").body)
+        puts @raw_info.inspect
+        @raw_info
+      rescue ::Errno::ETIMEDOUT
+        raise ::Timeout::Error
+      end
+    end
+  end
+end
